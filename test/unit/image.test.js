73b3732394af6df84d2974b8c83ae70b8f8b8d8d
Andrew Bodey <a.s.bodey@gmail.com>
2017-12-13 13:53:27 -0800

    added image unit test, and exp search test

---
+++ test/unit/image.test.js
@@ -0,0 +1,23 @@
+const { assert } = require('chai');
+const Image = require('../../lib/models/image');
+
+describe.only('Image Model', () => {
+    const image = new Image({
+        imageURI: 'home.vga'
+    });
+
+    it('should validate the Image model', () => {
+        assert.equal(image.validateSync(), undefined);
+    });
+
+    it('checks required fields', () => {
+        const badExp = new Image();
+        return badExp.validate()
+            .then(
+                () => {throw new Error('User validation error');},
+                ({ errors }) => {
+                    assert.equal(errors.imageURI.kind, 'required'); 
+                }
+            );
+    });
+});
