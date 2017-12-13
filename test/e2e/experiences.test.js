2c8fdd34c98aaa191dce5c626ac1b7ef32beda7b
Maryussuperboy <martsyalis@gmail.com>
2017-12-12 18:01:44 -0800

    refactoring change

--- test/e2e/experiences.test.js
+++ test/e2e/experiences.test.js
@@ -3,17 +3,20 @@ const { assert } = require('chai');
 const db = require('./db');
 
 describe('Experience API', () => {
-    let experience = null;
     let image = { imageURI:'http://i.dailymail.co.uk/i/pix/2016/09/06/11/37F60FD200000578-0-image-a-5_1473156426673.jpg', caption: 'rock' };
+    let experience =null;
 
     beforeEach(() =>{
         db.drop();
         return request.post('/api/images/')
             .send(image)
-            .then( ({ body }) => image = body);
+            .then( ({ body }) =>{
+                image = body;
+                experience = { title: 'Best', location: 'New York222', images:[image._id] };
+            });
+
     });
     it('/POST an experience', () => {
-        experience = {title: 'Best', location: 'New York', images:[image._id] };
         return request
             .post('/api/experiences')
             .send(experience)
@@ -26,7 +29,6 @@ describe('Experience API', () => {
     });
 
     it('/ Gets and populates exp by id', () => {
-        experience = {title: 'Best', location: 'New York', images:[image._id] };
         return request.post('/api/experiences')
             .send(experience)
             .then(({ body })=> {
@@ -41,13 +43,20 @@ describe('Experience API', () => {
     });
 
 
-    it.skip('/Delete experience', () => {
-        const url = `/api/experiences/${experience._id}`;
-        return request.delete(url)
-            .then(() => request.get(url))
+    it('/Delete experience', () => {
+        let id = null;
+        return request.post('/api/experiences')
+            .send(experience)
+            .then(({ body })=> {
+                id = body._id;
+                console.log('id is', id);
+                return request.delete(`api/experiences/${id}`);
+            })
+            .then(() => request.get(`api/experiences/${id}`))
             .then(
                 () => { throw new Error('unexpected success response'); },
                 res => assert.equal(res.status, 404)
             );
     });
+
 });
