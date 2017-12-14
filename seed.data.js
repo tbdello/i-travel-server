42ecc3dd0086bfc104a4208d63f0718965a17b0a
Andrew Bodey <a.s.bodey@gmail.com>
2017-12-14 13:44:43 -0800

    started with the profile pic

---
+++ seed.data.js
@@ -0,0 +1,47 @@
+========================USERS========================================
+[
+    { "name": "Andrew", "email": "andrew@mail.com" },
+    { "name": "Beth",   "email": "beth@mail.com"   },
+    { "name": "Carl",   "email": "carl@mail.com"   },
+    { "name": "Debbie", "email": "debbie@mail.com" },
+    { "name": "Eli",    "email": "edward@mail.com" },
+    { "name": "Felica", "email": "felica@mail.com" },
+    { "name": "George", "email": "george@mail.com" },
+    { "name": "Haily",  "email": "haily@mail.com"  },
+    { "name": "Irving", "email": "irving@mail.com" },
+    { "name": "Jill",   "email": "jill@mail.com"   },
+    { "name": "Keith",  "email": "keith@mail.com"  },
+    { "name": "Lisa",   "email": "lisa@mail.com"   },
+    { "name": "Mark",   "email": "mark@mail.com"   },
+    { "name": "Nacy",   "email": "nacy@mail.com"   },
+    { "name": "Opie",   "email": "opie@mail.com"   },
+    { "name": "Patty",  "email": "patty@mail.com"  }
+]
+
+
+========================EXPERIENCES==================================
+[
+    { "title": "Best1" , "location": "New1" , "tags":["hard", "soft"] },
+    { "title": "Best2" , "location": "New2" , "tags":["soft"] },
+    { "title": "Best3" , "location": "New3" , },
+    { "title": "Best4" , "location": "New4" , },
+    { "title": "Best5" , "location": "New5" , },
+    { "title": "Best6" , "location": "New6" , },
+    { "title": "Best7" , "location": "New7" , },
+    { "title": "Best8" , "location": "New8" , },
+    { "title": "Best9" , "location": "New9" , },
+    { "title": "Best10", "location": "New10", },
+]
+
+========================PICTURES=====================================
+
+
+
+
+
+
+
+
+
+
+
